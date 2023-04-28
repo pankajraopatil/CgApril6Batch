ad5cfc882c756fc95f4c867c99e40cd99f8fd9ce
Pankaj <Pankajrao_patil@hotmail.com>
2023-04-28 12:05:48 +0530

    new Class added to project with name Employee

---
+++ EMSProjectDemo/EMSProjectDemo/Employee.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace EMSProjectDemo
+{
+   public class Employee
+    {
+        public int EmployeeID { get; set; }
+        public string Name { get; set; }
+    }
+}
